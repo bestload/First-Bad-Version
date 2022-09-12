4d9dea1215347b6689cc84b0501a505072f5989e
bestload <egor.panyushkin@gmail.com>
2022-09-12 20:09:37 +0300

    ADD: isBadVersion and low firstBadVersion

--- src/source.cpp
+++ src/source.cpp
@@ -1,1 +1,19 @@
 #include "header.h"
+
+// support function for know answer
+bool isBadVersion(int unknVerNum, int badVerNum)
+{
+    return (unknVerNum == badVerNum);
+}
+
+int firstBadVersion(int n)
+{
+    /*
+        Этапы:
+            1. Проверить последнюю версию на ошибку
+            2. Проверить версию по середине на ошибку
+            3. Проверить версию предпоследнюю на ошибку
+            4. Проверить версию посередине середины на ошибку и т.д.
+    
+    */
+}
